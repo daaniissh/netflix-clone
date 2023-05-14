9e394d4783a1a746dbd787db8fbaf8c3a4b700cb
Danish <pkdanish329@gmail.com>
2023-05-14 13:11:56 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { origins, action } from "./urls";
+import Banner from "./components/Banner/Banner";
+import React, { useState } from "react";
+import Navbar from "./components/navbar/navbar";
+import { useEffect } from "react";
+// import RowPost from "./components/row-poster/RowPost";
 
 function App() {
+  // const [load, setLoad] = useState(true);
+  // const spinner = document.getElementById("spiner");
+  // const item = localStorage.setItem('load',true);
+
+  // useEffect(() => {
+  //   if (item) {
+  //     setTimeout(() => {
+  //             spinner.style.display = "none";
+  //             setLoad(false);
+  //           }, 1000);
+
+  //     setLoad(true);
+  //   } else {
+  //     setLoad(false);
+  //      localStorage.setItem('load', false);
+  //   }
+  // }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+        <Navbar />
+        <Banner />
+      </div>
+      )
+  
+  
 }
 
 export default App;
---
+++ src/contents/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = '64fce4404d5da5e36af21589af67c971'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
